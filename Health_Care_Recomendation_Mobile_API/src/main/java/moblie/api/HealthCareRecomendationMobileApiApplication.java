077022b9f37ba87dce8f40cb43634dcf4066ec3f
Nghinh Tri <tritnse130094@fpt.edu.vn>
2020-06-05 22:18:11 +0700

    Create project

---
+++ Health_Care_Recomendation_Mobile_API/src/main/java/moblie/api/HealthCareRecomendationMobileApiApplication.java
@@ -0,0 +1,13 @@
+package moblie.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareRecomendationMobileApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareRecomendationMobileApiApplication.class, args);
+	}
+
+}
