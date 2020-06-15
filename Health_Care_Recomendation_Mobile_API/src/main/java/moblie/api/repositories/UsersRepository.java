9868ed521d6de7124b4dcde439e83c0b86a1d29d
thinh <thinh@SE130021>
2020-06-15 16:07:48 +0700

    Api rating and recommendation

--- Health_Care_Recomendation_Mobile_API/src/main/java/moblie/api/repositories/UsersRepository.java
+++ Health_Care_Recomendation_Mobile_API/src/main/java/moblie/api/repositories/UsersRepository.java
@@ -2,7 +2,6 @@ package moblie.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import moblie.api.entities.Users;
