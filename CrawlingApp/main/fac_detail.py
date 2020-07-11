56e9999f5e1dcf75522b5b0524c75b9fc075f6c2
Jirou Tran <tritnse130094@fpt.edu.vn>
2020-07-11 20:20:15 +0700

    update python

--- CrawlingApp/main/fac_detail.py
+++ CrawlingApp/main/fac_detail.py
@@ -1,10 +1,11 @@
-from main import symptom_main as Symptom
 from connection import connection
 from bs4 import BeautifulSoup
-import requests
 from util import util
-import string
+import logging
+from googletrans import Translator
 
+logger = logging.Logger('catch_all')
+translator = Translator()
 
 def get_specialty(symptom):
     html = "https://en.wikipedia.org/wiki/" + symptom
@@ -14,22 +15,59 @@ def get_specialty(symptom):
     specialty = soup.select('.infobox tbody tr td a[title]')
     for i in specialty:
         if connection.checkDuplicateSpec(str(i.string)):
-            # print("Spec: " + i.get('title'))
             return connection.getSpecialitiesByName(i.get('title'))
 
+def findAllFacilities(url):
+    try:
+        text=util.getHtml(url)
+        soup=BeautifulSoup(text,'html.parser')
+        for link in soup.select("h3 > a"):
+            string=link.get('title')
+            print(string)
+    except:
+        print("error")
+        logger.error()
 
+
+def getFacilitiesBySpec(speciality):
+    try:
+        url='https://timbenhvien.vn/'
+        text=util.getHtml(url)
+        soup=BeautifulSoup(text,'html.parser')
+        for link in soup.find_all('a'):
+            title=link.string
+            href=link.get('href')
+            if title != None:
+                trans=translator.translate(title, dest='en').text
+                if speciality in trans:
+                    print("spec: " +trans)
+                    print(href)
+                    # findAllFacilities(href)
+            # set.add(title)
+        # for x in set:
+        #     print(x)
+        #     trans = translator.translate(x, dest='vi').text
+        #     print(trans)
+        #     connection.insertSymp(x,trans)
+    except:
+        print("error")
+        logger.error()
 
 if __name__ == '__main__':
     symptoms = connection.getAllSymptom()
-    # specialities = connection.getAllSpecialities()
+    # specialities = connection.getSpecialitiesByName()
 
     for symp in symptoms:
         id = symp[0]
         symptom = symp[1]
         translate = symp[2]
-        # print("symptom: " + symptom)
+        print("symptom: " + symptom)
+
         spec = get_specialty(symptom)
         if (spec != None and len(spec) > 0):
-            # print(spec[0][0])
-            print(spec)
-
+            specId = spec[0][0]
+            speciality = spec[0][1]
+        else:
+            specId = '229'
+            speciality = 'General Surgery'
+        getFacilitiesBySpec(speciality)
