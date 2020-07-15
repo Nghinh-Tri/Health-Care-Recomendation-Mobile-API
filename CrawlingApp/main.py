64ebd09776219ac9df8a868ac76c0b0129120ad8
Jirou Tran <tritnse130094@fpt.edu.vn>
2020-07-15 11:22:00 +0700

    update api

--- CrawlingApp/main.py
+++ CrawlingApp/main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     factory.crawlingFactory("http://www.medinet.gov.vn/DSDonvi.aspx?codekhoi=BVTP",
                     "html\BVTP.html")
     factory.crawlingFactory("http://www.medinet.gov.vn/DSDonvi.aspx?codekhoi=BVQH",
-                            "html\BBVQH.html")
+                            "html\BVQH.html")
     speciality.crawlingSpeciality("https://www.aamc.org/cim/explore-options/specialty-profiles",
                        "html\specialty-profiles.html")
     symptom.crawlingSymptom("https://en.wikipedia.org/wiki/List_of_medical_symptoms")
---
+++ mobile_healthcare/lib/database/repository_sqlite.dart
@@ -0,0 +1,46 @@
+import 'package:mobile_healthcare/database/sqlitedb.dart';
+import 'package:mobile_healthcare/model/facility/facility.dart';
+
+class RepositoryServiceSQLite {
+  static Future<List<Facility>> getAllFacility() async {
+    final select = '''SELECT * FROM ${DatabaseCreator.table}''';
+    final data = await DatabaseCreator.db.rawQuery(select);
+    List<Facility> result = List();
+
+    for (final fac in data) {
+      final facility = Facility.fromJson(fac);
+      result.add(facility);
+    }
+    return result;
+  }
+
+  static Future<Facility> getFacility(int id) async {
+    final select =
+        '''SELECT * FROM ${DatabaseCreator.table} WHERE ${DatabaseCreator.id} == $id''';
+    final data = await DatabaseCreator.db.rawQuery(select);
+    final fac = Facility.fromJson(data[0]);
+    return fac;
+  }
+
+  static Future<void> addFacility(Facility facility) async {
+    final sql = '''INSERT INTO ${DatabaseCreator.table} 
+    (
+      ${DatabaseCreator.id},${DatabaseCreator.name},${DatabaseCreator.address},${DatabaseCreator.phone},
+      ${DatabaseCreator.latitude},
+      ${DatabaseCreator.longtitude},
+      ${DatabaseCreator.image}
+    )
+    VALUES
+    (
+      ${facility.id},
+      ${facility.name},
+      ${facility.address},
+      ${facility.phone},
+      ${facility.latitude},
+      ${facility.longtitude},
+      ${facility.image}
+    )''';
+
+    final result = await DatabaseCreator.db.rawInsert(sql);
+  }
+}
